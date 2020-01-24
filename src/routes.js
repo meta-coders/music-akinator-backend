aef7cc13caa727bada0edf760d8b1a224f25afac
Rumiantsev Oleksii <rumiantsev.oleksii@gmail.com>
2020-01-24 10:23:44 +0200

    feat: implement recognize by lyrics

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const recognizeByHummingHandler = require('./recognizeByHumming');
+const recognizeByLyricsHandler = require('./recognizeByLyrics');
 
 const routes = async (fastify, options) => {
-  fastify.post('/recognizeByHumming', recognizeByHummingHandler);
+  fastify.post('/recognizeByLyrics', recognizeByLyricsHandler);
+  fastify.get('/recognizeByLyrics', recognizeByLyricsHandler);
 };
 
 module.exports = routes;
