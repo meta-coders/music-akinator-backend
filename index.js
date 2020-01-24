6bfef427bf731ede09b96cc7c4b847327489fed4
Rumiantsev Oleksii <rumiantsev.oleksii@gmail.com>
2020-01-24 20:42:21 +0200

    feat: add fastify-static

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 'use strict';
 
+const path = require('path');
 const fastify = require('fastify')();
 
 fastify.register(require('fastify-multipart'));
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public'),
+});
 fastify.register(require('./src/routes'));
 
 fastify.listen(3000).catch(console.error);
