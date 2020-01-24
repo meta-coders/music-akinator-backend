01c07133334e7bbf076db8176c80b8c7786d23b8
Rumiantsev Oleksii <rumiantsev.oleksii@gmail.com>
2020-01-24 21:20:04 +0200

    feat: add host to listen parameters

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ fastify.register(require('fastify-static'), {
 });
 fastify.register(require('./src/routes'));
 
-fastify.listen(process.env.PORT).catch(console.error);
+fastify.listen(process.env.PORT, '0.0.0.0').catch(console.error);
