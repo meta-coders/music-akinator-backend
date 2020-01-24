cbda34727caf120c067ddcad19cf441d7edf91a8
Rumiantsev Oleksii <rumiantsev.oleksii@gmail.com>
2020-01-24 21:01:09 +0200

    feat: use env variable PORT for server deployment

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ fastify.register(require('fastify-static'), {
 });
 fastify.register(require('./src/routes'));
 
-fastify.listen(3000).catch(console.error);
+fastify.listen(process.env.PORT).catch(console.error);
