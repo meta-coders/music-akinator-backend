05fe03500bc56a7c21bbb94523959bcf076c5855
Rumiantsev Oleksii <rumiantsev.oleksii@gmail.com>
2020-01-24 21:26:29 +0200

    feat: add fastify-cors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const fastify = require('fastify')();
 
 fastify.register(require('fastify-multipart'));
+fastify.register(require('fastify-cors'));
 fastify.register(require('fastify-static'), {
   root: path.join(__dirname, 'public'),
 });
