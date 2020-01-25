38ce68b3e12fabf83f1a219eea8ed259c0f2a392
mille-nium <dianaboloten.db@gmail.com>
2020-01-25 20:08:15 +0200

    chore: update public data

---
+++ public/precache-manifest.bfd83d52446d5b1f3008519d72bbf657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a65acce1292704c7b166f7fec7025dce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01d65a9a18b6de4e73d1",
+    "url": "/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "0fe6f195f78da5ab5b1f",
+    "url": "/static/js/2.e6e8a548.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.e6e8a548.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01d65a9a18b6de4e73d1",
+    "url": "/static/js/main.04639f9b.chunk.js"
+  },
+  {
+    "revision": "bef4cc72f3a15a30d57e",
+    "url": "/static/js/runtime-main.40daf685.js"
+  }
+]);
